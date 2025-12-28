d7ef8aeb45e4a7c6973bee94e7317f80189bc331
JI-LAB-2025 <jilab2025@gmail.com>
2025-12-28 14:10:01 +0000

    Deploying to gh-pages from @ JI-LAB-2025/ji-lab-2025.github.io@67b60e57af79accbea59c73063c51c40b1a51c61 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "JI-LAB-2025/JILAB2025",
+    "data-repo": "JI-LAB-2025/ji-lab-2025.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
